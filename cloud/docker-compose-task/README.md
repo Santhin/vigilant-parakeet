336d3b3e33245c2a91d71f93118b8fe138c1dae8
Piotr Sierkin <psierkin@gmail.com>
2021-09-26 17:55:24 +0200

    Structural changes 2

---
+++ cloud/docker-compose-task/README.md
@@ -0,0 +1,17 @@
+# docker compose task
+```
+Aplikacja zawiera trzy moduły:
+ - backend
+ - bazę danych
+ - elasticsearch
+ - kibana
+1. Zrób Dockerfile do zbudowania obrazu backendu
+    a) Backennd powinien mieć dwa endpointy
+    - jeden dla bazy, żeby zwrócić tablice
+    - drugi dla elasticsearch, żeby zwrócić index
+2. Użyj docker composa
+    a) Połącz cztery serwisy
+    b) spraw żeby były widoczne mędzysobą
+    c) niech backend odpali się na samym końcu
+3. Zapisuj logi na swoim dysku
+```
---
+++ cloud/docker-compose-task/src/config.py
@@ -0,0 +1,12 @@
+import os
+import urllib
+
+host_server = os.environ.get('POSTGRES_HOST', 'localhost')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('POSTGRES_PORT', '15432')))
+database_name = os.environ.get('POSTGRES_DB', 'starcraft')
+db_username = urllib.parse.quote_plus(str(os.environ.get('POSTGRES_USER', 'pgsync')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('POSTGRES_PASSWORD', 'qazwsx123')))
+
+
+DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode=prefer'.format(db_username,db_password, host_server, db_server_port, database_name)
+ELASTICSEARCH_URL = f"{os.environ.get('ELASTICSEARCH_HOST', 'localhost')}:{os.environ.get('ELASTICSEARCH_PORT', '9201')}"
